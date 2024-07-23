8f7d9a9a1ecdbfceb67bde8324bdc712dd7ecd2f
Alexey Kachanov <newmylife88@gmail.com>
2024-07-23 17:06:53 +0400

    final commit

--- src/widget.py
+++ src/widget.py
@@ -14,9 +14,19 @@ def mask_account_card(card_number: str) -> str:
         return mask
 
 
+    def get_date(data: Any) -> Any:
+        """Функция которая возвращает дату"""
+        if data == "":
+            return ""
+
+        d = datetime.strptime(data, format("%Y-%m-%dT%H:%M:%S.%f"))
+        return d.strftime("%d.%m.%Y")
+
+
 
 
 
 print(mask_account_card("Счет 73654108430135874305"))
 print(mask_account_card("Visa Platinum 7000792289606361"))
 print(mask_account_card("Maestro 7000792289606361"))
+print(get_date("2024-03-11T02:26:18.671407"))
